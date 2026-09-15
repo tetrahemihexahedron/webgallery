bac99ce4178dab9d26530c6231aff1a090d70e80
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-09-14 21:27:47 -0600

    Call Render in cmd/gallery

--- cmd/gallery/main.go
+++ cmd/gallery/main.go
@@ -1,9 +1,50 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"tetrahemihexahedron/webimage/internal/gallery"
+)
 
 func main() {
-	if _, err := loadConfig(); err != nil {
+	cfg, err := loadConfig()
+	if err != nil {
 		log.Fatal(err)
 	}
+
+	if err := renderGallery(cfg); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func renderGallery(cfg Config) (err error) {
+	writer := io.Writer(os.Stdout)
+
+	if !cfg.UseStdout {
+		file, err := os.Create(cfg.OutFile.String())
+		if err != nil {
+			return fmt.Errorf("opening output %q: %w", cfg.OutFile, err)
+		}
+		defer func() {
+			if closeErr := file.Close(); err == nil && closeErr != nil {
+				err = fmt.Errorf("closing output %q: %w", cfg.OutFile, closeErr)
+			}
+		}()
+
+		writer = file
+	}
+
+	opts := gallery.Options{
+		ImagesRoot: cfg.ImagesRoot,
+		URLPrefix:  cfg.URLPrefix,
+		Sort:       cfg.Sort,
+	}
+	if err := gallery.Render(writer, opts); err != nil {
+		return fmt.Errorf("rendering gallery: %w", err)
+	}
+
+	return nil
 }
